1908ce49cb8a79ec377479900c28b5142b6b01e5
lzyssg <2591539404@qq.com>
2022-11-12 02:45:33 +0800

    3完成后台构建

---
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.entity.User;
+import com.example.springboot.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin//解决跨域
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    IUserService userService;
+
+    @GetMapping("/list")
+    public List<User> listUsers(){
+        return userService.listUsers();
+    }
+}
